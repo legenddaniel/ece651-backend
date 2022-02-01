3b3084d5b98f98902857fe21ab613efe1a9d99d3
legenddaniel <53063815+legenddaniel@users.noreply.github.com>
2022-01-31 22:25:52 -0500

    API tests. Error with signout test.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 from model_utils.models import UUIDModel, TimeStampedModel
 
